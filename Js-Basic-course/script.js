c709744169a077f34a945c6401143cb04ff4bfb6
KomilGuliev <komil-guliev@mail.ru>
2019-02-28 20:03:29 +0300

    Starting to course section: Javascript Basics

---
+++ Js-Basic-course/script.js
@@ -0,0 +1,60 @@
+
+/*********************************************
+ * Variables and data types
+ */
+
+/*
+console.log("Hello world!!!");	//Консольный вывод
+
+var firstName = 'Kanda';
+console.log(firstName);		//Kanda
+
+var lastName = 'Smith';
+var age = 30;
+
+var fullAge = true;
+console.log(fullAge);		//true
+
+//We you camelCase style for declaring variables
+
+var job;
+console.log(job);		//undefined
+
+job = 'Teacher';
+console.log(job);		//Teacher
+
+
+//var 3yaers = 'Something'		Unexpected token
+//var &years = 'Something'		Unexpected token &
+//Reserved words like 
+//function, if, delete..
+//we can't use it as variable name
+
+
+ var _3years = 'some..';
+ console.log(_3years);		//some.. 
+
+*/
+
+
+
+
+/*********************************************
+ * Variable mutation and type coertion
+ */
+
+
+ //Type coertion
+ var firstName = 'John';
+ var age = 28;
+ var isMarried = false;
+
+ console.log(firstName + ' ' + age + ' Is he married? ' + isMarried);
+
+ //Variable mutation
+ age = 'twenty eight';
+ job = 'driver';
+
+ alert(firstName + ' ' + age + ' Is he married? ' + isMarried);
+ var lastName = prompt('What is his last Name?');
+ console.log(lastName);
